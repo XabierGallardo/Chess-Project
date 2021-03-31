2e00081b23d0a1ce6db22759841264d152f2d297
XabierGallardo <javiergallard91@gmail.com>
2021-03-31 23:58:48 +0200

    Defined structures and sliding on moves

--- moves.cs
+++ moves.cs
@@ -27,3 +27,61 @@ static PrecomputedMoveData () {
 		}
 	}
 }
+
+
+
+//Defining a structure for holding a move that will record the target square of the piece we want to move
+public struct Move {
+	public readonly int StartSquare;
+	public readonly int TargetSquare;
+
+	List <Move> moves;
+
+	public List<Move> GenerateMoves () {
+
+		moves = new List<Move> ();
+
+		//Looping over all 64 squares and see what piece is on each square
+		for(int startSquare = 0; startSquare < 64l startSquare ++) {
+
+			int piece = Board.Square[startSquare];
+
+			//We're only interested in the piece if it's the right colour for whoever's turn it is to move
+			if( Piece.IsColour (piece, Board.ColourToMove)) {
+				if(Piece.isSlidingPiece(piece)) {
+					GenerateSlidingMoves (startSquare, piece);
+				}
+			}
+		}
+
+		return moves;
+	}
+}
+
+
+
+//In the sliding piece function, let's loop over 8 different directions
+//for each direction, we'll also have a loop for the number of squares that exist in that direction, up to the edge of the board
+void GenerateSlidingMoves (int startSquare, int piece) {
+	int startDirIndex = (Piece.IsType (piece, Piece.Bishop)) ? 4 : 0;
+	int endDirIndex = (Piece.IsType (piece, Piece.Rook)) ? 4 : 8;
+
+	for(int directionIndex = startDirIndex; directionIndex < enDirIndex; directionIndex ++) {
+		for (int n = 0; NumSquaresToEdge[startSquare][directionIndex]; n++) {
+			int targetSquare = startSquare + DirectionOffsets[directionIndex] * (n + 1);
+			int pieceOnTargetSquare = Board.Square[targetSquare];
+
+			//Blocked by friendly piece, so can't move any further in this direction
+			if(Piece.IsColour (pieceOnTargetSquare, friendlyColour)) {
+				break;
+			}
+
+			move.Add (new Move (startSquare, targetSquare));
+
+			//Can't move any further in this direction after capturing opponent's piece
+			if (Piece.IsColour (pieceOnTargetSquare, opponentColour)) {
+				break;
+			}
+		}
+	}
+}
